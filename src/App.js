f722677384df491b59c27f992d9d9420303439fc
jlcdmp <joeoliverc123@gmail.com>
2019-08-02 21:13:20 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './Components/navigation';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' >
+
+      <h1> supa-loans</h1>
+      <div className='navigation'>
+
+        <Navigation />
+
+      </div>
+
+      <div className='content'>
+        <p>content</p>
+      </div>
+
+
     </div>
+
   );
 }
 
